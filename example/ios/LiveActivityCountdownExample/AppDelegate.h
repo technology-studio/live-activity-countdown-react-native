51b55ee44bd670089ba06066e38de30dd5a1d0b7
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2023-04-04 19:32:56 +0200

    feat: add initial implementation (#2)
    * feat: add initial implementation
    
    * fix: move example index outside src
    
    * feat: move view podspec to different package
    
    * fix: use newer ios version for LiveActivityCountdown podspec
    
    * chore: bump deployment targets in pods and example tests
    
    * fix: install LiveActivityCountdownView from node_modules
    
    * chore: add functional to dev dependencies to fix eslint issues
    
    * feat: add dependency for view package
    
    * fix: allow podspec to be published on npm
    
    * style: add missing ending line
    
    * fix: add `ios` folder to be published as well
    
    * fix: add missing nested folders
    
    * chore: remove unused test
    
    * chore: remove unwanted workflow
    
    * chore: update `@txo/live-activity-countdown-view-react-native`
    
    ---------
    
    Co-authored-by: Rostislav Simonik <rostislav.simonik@technologystudio.sk>

--- example/ios/LiveActivityCountdownExample/AppDelegate.h
+++ example/ios/LiveActivityCountdownExample/AppDelegate.h
@@ -1,5 +1,10 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import "LiveActivityCountdownExample-Swift.h"
+//#import <LiveActivityCountdown-Swift.h>
+//#import <LiveActivityCountdown/LiveActivityCountdown-Swift.h>
+//#import "LiveActivityCountdown/LiveActivityCountdown-Swift.h"
+//#import "LiveActivityCountdown-Swift.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,76 @@
-import { NativeModules, Platform } from 'react-native';
+import {
+  NativeModules, Platform,
+} from 'react-native'
 
 const LINKING_ERROR =
-  `The package 'react-native-live-activity-countdown' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+  `The package '@txo/live-activity-countdown-react-native' doesn't seem to be linked. Make sure: \n\n${
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' })
+  }- You rebuilt the app after installing the package\n` +
+  '- You are not using Expo Go\n'
 
-const LiveActivityCountdown = NativeModules.LiveActivityCountdown
+const LiveActivityCountdown = NativeModules.LiveActivityCountdown != null
   ? NativeModules.LiveActivityCountdown
   : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+    {},
+    {
+      get () {
+        throw new Error(LINKING_ERROR)
+      },
+    },
+  )
 
-export function multiply(a: number, b: number): Promise<number> {
-  return LiveActivityCountdown.multiply(a, b);
+export type LiveActivityConfig = {
+  title: string,
+  timerTitle: string,
+  endDateTime: string | Date,
+  timerColor?: string,
+  imageName?: string,
+}
+
+export async function createLiveActivity (
+  config: LiveActivityConfig,
+): Promise<string | undefined> {
+  if (Platform.OS === 'android') {
+    if (__DEV__) {
+      // eslint-disable-next-line no-console
+      console.warn('LiveActivityCountdown is not available on Android')
+    }
+    await Promise.resolve(undefined); return
+  }
+  const { title, timerTitle, endDateTime, timerColor, imageName } = config
+  const secondsUntilEnd = Math.floor(
+    (new Date(endDateTime).getTime() - new Date().getTime()) / 1000,
+  )
+  if (secondsUntilEnd < 0) {
+    throw new Error('endDateTime must be in the future')
+  }
+  return LiveActivityCountdown.createLiveActivity({
+    title,
+    timerTitle,
+    secondsUntilEnd,
+    timerColor,
+    imageName: imageName ?? null,
+  })
+}
+
+export async function endLiveActivity (id: string): Promise<void> {
+  if (Platform.OS === 'android') {
+    if (__DEV__) {
+      // eslint-disable-next-line no-console
+      console.warn('LiveActivityCountdown is not available on Android')
+    }
+    await Promise.resolve(undefined); return
+  }
+  return LiveActivityCountdown.endLiveActivity(id)
+}
+
+export async function getLiveActivities (): Promise<string[] | undefined> {
+  if (Platform.OS === 'android') {
+    if (__DEV__) {
+      // eslint-disable-next-line no-console
+      console.warn('LiveActivityCountdown is not available on Android')
+    }
+    await Promise.resolve(undefined); return
+  }
+  return LiveActivityCountdown.getLiveActivities()
 }
