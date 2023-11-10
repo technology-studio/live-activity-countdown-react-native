7a3844588f832b5fdf3e895ece4c0e2272611152
Erik Slovak <erik.slovak@technologystudio.sk>
2023-11-10 15:16:11 +0100

    chore: add newline

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,8 @@ const txoConfig = require('eslint-config-txo-typescript')
 /** @type {import('eslint').Linter.FlatConfig[]} */
 const config = [
   ...txoConfig.default,
-  {    ignores: [
+  {
+    ignores: [
       'babel.config.js',
       'example/babel.config.js',
       'example/index.js',
