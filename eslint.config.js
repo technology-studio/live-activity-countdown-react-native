e43ce5fafd07bf0911bf8e2ecc3b11bcf5c01fe2
Erik Slovak <erik.slovak@technologystudio.sk>
2023-11-10 13:33:45 +0100

    feat: migrate to flat eslint config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+const txoConfig = require('eslint-config-txo-typescript')
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const config = [
+  ...txoConfig.default,
+  {    ignores: [
+      'babel.config.js',
+      'example/babel.config.js',
+      'example/index.js',
+      'example/metro.config.js',
+      'example/node_modules',
+      'example/react-native.config.js',
+      'scripts',
+    ],
+  },
+]
+
+module.exports = config
